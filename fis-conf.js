f839917fc13fcbc9a4dfc97cb2badef30b80f6b5
Alex <y.alex@cjatech.com>
2016-09-30 16:22:58 +0800

    文件更新 1、根据格式化字符串控制月和日选项是否显示

---
+++ fis-conf.js
@@ -0,0 +1,79 @@
+fis.config.merge({
+	project: {
+		include:["src/**"],
+		fileType: {
+			text: ["es6", "shtml"]
+		}
+	},
+	modules: {
+		postpackager: "simple",
+		postprocessor: {
+			css: "autoprefixer"
+		},
+		spriter: "csssprites",
+		parser: {
+			less : "less",
+			sass : "node-sass",
+			scss : "node-sass",
+			jsx  : ["babel-5.x", "react"],
+			es6  : "babel-5.x"
+		}
+	},
+	pack: {
+		"pkg/css/basic.css": [
+			"css/base/reset.css",
+			"css/base/base.scss",
+		],
+		"pkg/css/main.css": [
+			"css/fonts/icomoon/style.css",
+			"css/struct/custom.scss",
+			"css/struct/layout.scss"
+		],
+		"pkg/js/lib.js": [
+			"js/jquery/jquery-1.11.2.js",
+			"js/fastclick/fastclick.js"
+		]
+	},
+	settings: {
+		spriter: {
+			csssprites: {
+				margin: 5
+			}
+		},
+		parser: {
+			"babel-5.x": {
+				blacklist: ["regenerator"],
+				optional: ["es7.classProperties"],
+				stage: 3,
+				sourceMaps: true
+			}
+		},
+		postprocessor: {
+			autoprefixer: {
+				browsers: ["Android >= 2.3", "ChromeAndroid > 1%", "iOS >= 4"],
+				cascade: true
+			}
+		},
+		optimizer: {
+			"uglify-js": {
+				sourceMap: true
+			}
+		}
+	},
+	roadmap: {
+		ext: {
+			less : "css",
+			sass : "css",
+			scss : "css",
+			jsx  : "js",
+			es6  : "js"
+		},
+		path: [
+			{
+				reg: /\/src\/(.*?)\.(.*?)/i,
+				release: "$1",
+				url: "$1",
+			}
+		]
+	}
+});
---
+++ fis-debug.cmd
@@ -0,0 +1,6 @@
+@echo off
+if not exist fis-conf.js echo fis.config.merge({project:{exclude:["*.sln","*.suo","*.config"],}}); > fis-conf.js
+cd %cd%
+del /f /q "./dist"
+cmd /c fis release -d "./dist" -wL
+pause
